f3d795d2b5dd7c91920410d8ea52247b7c3a0649
arcade15xyz <arcade15xyz@gmail.com>
2025-09-04 12:24:47 +0530

    swapped Favourite button

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -5,7 +5,6 @@ import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
-import 'package:meals_app/providers/meals_provider.dart';
 import 'package:meals_app/providers/favorites_provider.dart';
 import 'package:meals_app/providers/filters_provider.dart';
 
